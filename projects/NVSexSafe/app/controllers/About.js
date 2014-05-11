23fbedf757ee2a2e8d66103a5e73b85de77cb6d9
Joe Chavez <joe@izen.me>
2014-05-10 17:35:58 -0700

    Both Items 13 and 16
    Add disclaimer label

--- projects/NVSexSafe/app/controllers/About.js
+++ projects/NVSexSafe/app/controllers/About.js
@@ -1,5 +1,3 @@
-
-
 function showLeavingAppAlert(targetUrl) {
 	if (OS_ANDROID) {
 		var msg = "You are now leaving the SexSafe app and visiting " + targetUrl + ". To return to the app, press the back button on your phone.";
@@ -9,13 +7,13 @@ function showLeavingAppAlert(targetUrl) {
 	}
 
 	var dialog = Ti.UI.createAlertDialog({
-    	cancel: 1,
-    	buttonNames : ['Continue', 'Cancel'],
+		cancel : 1,
+		buttonNames : ['Continue', 'Cancel'],
 		message : msg,
 		title : Ti.App.name
 	});
 	dialog.addEventListener('click', function(e) {
-		if (e.index !== e.source.cancel){
+		if (e.index !== e.source.cancel) {
 			Ti.Platform.openURL(targetUrl);
 		}
 	});
@@ -43,6 +41,8 @@ function open() {
 	$.copyrightLabel.text += Ti.App.copyright;
 	$.publisherLabel.text += Ti.App.publisher;
 	$.urlLabel.text += Ti.App.url;
+
+	$.disclaimerLabel.text = 'Legal Disclaimer\nPlease read this agreement entirely and carefully before accessing this web site. By accessing the site, you agree to be bound by the terms and conditions below. If you do not wish to be bound by these terms and conditions, you may not access or use this site or its associated applications (NVSexSafe/NVSelfCare applications for Android and iOS).\n\nNo medical advice.\nThe information posted here should not be considered medical advice and is not intended to replace consultation with a qualified healthcare professional.\n\nNo warranties.\nThis web site and its associated applications are provided on an "as is" on an "as available" basis without warranties of any kind, express or implied, including, but not limited to, those of title, merchantability, fitness for a particular purpose or non-infringement or any warranty arising from a course of dealing, usage, or trade practice. No oral advice or written information provided by Universal Web Solutions Canada shall create a warranty; nor shall members or visitors to the site and users of its associated applications rely on any such information or advice.\n\nDisclaimer of Liability.\nThe user assumes all responsibility and risk for the use of this web site and its associated applications. Under no circumstances, including negligence, shall the Nevada Coalition for Safe Sex or anyone else involved in creating or maintaining this web site and its associated applications be liable for any direct, indirect, incidental, special or consequential damages, that may result from the use or inability to use the web site and its associated applications. Nor shall Nevada Coalition for Safe Sex be liable for any such damages including, but not limited to, reliance by a member or visitor on any information obtained via the web site and its associated applications; or that result from mistakes, omissions, interruptions, deletion of files, viruses, errors, defects, or any failure of performance, communications failure, theft, destruction or unauthorized access. In jurisdictions where aforementioned limitations on liability are not allowed to the extent described, our liability shall be limited to the greatest extent allowed by law.\n\nTHESE LEGAL NOTICES ARE SUBJECT TO CHANGE WITHOUT NOTICE.';
 
 	if (OS_ANDROID) {
 		$.navWin.activity.addEventListener('stop', stopActivityAndroid);
